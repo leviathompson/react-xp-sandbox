513aba13a2e6114d79b110f6c00da54c0de944b3
Cyanoxide <cyanoxide@gmail.com>
2026-02-15 22:46:46 +0000

    Add logic for windows with no icon

--- frontend/src/components/Window/Window.tsx
+++ frontend/src/components/Window/Window.tsx
@@ -243,8 +243,8 @@ const Window = ({ ...props }: WindowProps) => {
                 <div className="w-full h-full pointer-events-none">
                     <div ref={titleBarRef} className={`${styles.titleBar} flex justify-between pointer-events-auto`} data-label="titlebar" onPointerDown={onTitleBarPointerDown} onDoubleClick={() => toggleMaximizeWindow(activeWindow)}>
                         <div className="flex items-center">
-                            <img src={icon || iconLarge} width="14" height="14" className="mx-2 min-w-[14px]"></img>
-                            <h3>{title}</h3>
+                            {(icon || iconLarge) && <img src={icon || iconLarge} width="14" height="14" className="mx-2 min-w-[14px]"></img>}
+                            <h3 className={(icon || iconLarge) ? "" : "ml-2"}>{title}</h3>
                         </div>
                         <div className="flex">
                             {resizable && (
